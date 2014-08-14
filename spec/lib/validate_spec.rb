2610a36e5712d9e65f6ca7462d0ce60c0d2bd921
Cath Thomas <goishi.san@gmail.com>
2014-08-14 03:23:38 -0400

    Handle empty strings, nil values, and nonexistant hash keys in Validator

--- spec/lib/validate_spec.rb
+++ spec/lib/validate_spec.rb
@@ -4,54 +4,76 @@ describe Echochamber::Validate do
 
   describe "#user" do
 
-    let(:first_name)   { "John" }
-    let(:last_name)    { "Public" }
-    let(:password)     { "password_on_the_sticky_note" }
-    let(:email)        { "johnqpublic@comcast.net" }
-    let(:phone)        { "885551234" }
+    let(:first_name)  { "John" }
+    let(:last_name)   { "Public" }
+    let(:password)    { "superhardpassword" }
+    let(:email)       { 'john.q.public@comcast.net' }
 
     let(:user) do
       {
-        first_name: first_name,
-        last_name:  last_name,
-        email:      email,
-        password:   password,
-        phone:      phone
+        firstName:   first_name,
+        lastName:    last_name,
+        email:       email,
+        password:    password,
+        phone:       "885551234" 
       }
     end
+
     let(:validation) { Echochamber::Validate.user(user) }
 
-    context "when it has no first name" do
-      let(:first_name) { '' }
+    context "when email is nil " do
+      let(:email) { nil }
 
-      it "raises an invalid credential error" do
-        expect{validation}.to raise_error(Echochamber::InvalidCredentialError)
+      it "raises an invalid parameter error" do
+        expect{validation}.to raise_error(Echochamber::InvalidParameterError)
       end
     end
+
+    context "when email is empty" do
+      let(:email) { '' }
+
+      it "raises an invalid parameter error" do
+        expect{validation}.to raise_error(Echochamber::InvalidParameterError)
+      end
+    end
+
+    context "when email key is not present" do
+      let(:email_less_user) do
+        no_email = user.dup 
+        no_email.delete(:email)
+        no_email
+      end
+      it "raises an invalid parameter error" do
+        expect{Echochamber::Validate.user(email_less_user)}.to raise_error(Echochamber::InvalidParameterError)
+      end
+    end
+
+
 
     context "when it has no password" do
       let(:password) { '' }
 
-      it "raises an invalid credential error" do
-        expect{validation}.to raise_error(Echochamber::InvalidCredentialError)
+      it "raises an invalid parameter error" do
+        expect{validation}.to raise_error(Echochamber::InvalidParameterError)
       end
     end
 
     context "when it has no last name" do
       let(:last_name) { '' }
 
-      it "raises an invalid credential error" do
-        expect{validation}.to raise_error(Echochamber::InvalidCredentialError)
+      it "raises an invalid parameter error" do
+        expect{validation}.to raise_error(Echochamber::InvalidParameterError)
       end
     end
 
-    context "when it has no email" do
-      let(:email) { '' }
+    context "when it has no first name" do
+      let(:first_name) { '' }
 
-      it "raises an invalid credential error" do
-        expect{validation}.to raise_error(Echochamber::InvalidCredentialError)
+      it "raise an invalid parameter error" do
+        expect{validation}.to raise_error(Echochamber::InvalidParameterError)
       end
     end
+
   end
 
 end
