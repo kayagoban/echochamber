d8b3b1e5ba1cb029b34839036d6183d7b10e9294
Cath Thomas <goishi.san@gmail.com>
2014-08-16 02:50:37 -0400

    Multiple changes
    - Refactor Request module
    - Successful user creation

---
+++ lib/echochamber/request.rb
@@ -0,0 +1,39 @@
+require 'rest-client'
+
+module Echochamber::Request
+
+  BASE_URL          = 'https://secure.echosign.com/api/rest/v2'
+
+  ENDPOINT = { 
+    token: BASE_URL + '/auth/tokens',
+    user: BASE_URL + '/users'
+  }
+
+  # Retrieves the authentication token
+  #
+  # @param credentials [Echochamber::Credentials] Initialized Echochamber::Credentials
+  # @return [String] Valid authentication token
+  def self.get_token(credentials)
+    response = RestClient.post(ENDPOINT[:token], credentials.to_json, :content_type => :json, :accept => :json)
+    response_body = JSON.parse(response.body)
+    response_body.fetch("accessToken")
+  end
+
+  # Performs REST create operation
+  #
+  # @param endpoint_key [Symbol] Symbol to resolve the Echochamber::Request::ENDPOINT hash.  Can be one of :tokens, :users, :blah, :blah, :etc
+  # @param body [Hash] Valid request body
+  # @return [String] Valid authentication token
+  def self.create(endpoint_key, body, token)
+    require 'pry'; binding.pry
+    response = RestClient.post(
+      ENDPOINT.fetch(endpoint_key), 
+      body.to_json, 
+      { :content_type => :json, :accept => :json, 'Access-Token' => token}
+    )
+    JSON.parse(response.body)
+  end
+
+
+end
+
--- spec/lib/client_spec.rb
+++ spec/lib/client_spec.rb
@@ -2,12 +2,43 @@ require 'spec_helper'
 
 describe Echochamber::Client do
 
-  describe '.initialize' do
+  let (:user_params) do
+    {
+      firstName:  'JohnQ',
+      lastName:   'Public',
+      email:      'john.public@comcast.net',
+      password:   'kN12oK9p!3',
+      title:      'Hedge Wizard'
 
+    }
+  end
+  let(:app_id)        { "9QCM5C6Y2AX82M" }
+  let(:app_secret)    { "390db093ee6f71ffc6d488b9457593a7" }
+  let(:api_key)       { "XCNLJFG83URX95Y" }
+  let(:email)         { "cthomas@railjumper.com" }
+  let(:password)      { "vvidmk" }
+
+  let(:credentials) do  
+    Echochamber::Credentials.build(app_id, app_secret, api_key, email, password)
+  end
+
+  let(:client) do
+    Echochamber::Client.new(credentials) 
+  end
+  
+
+  describe '#initialize' do
+    it 'returns self' do
+      expect(client.token).to_not eq(nil) 
+    end
   end
 
   describe '.create_user' do
-
+    it 'calls validator' do
+      user = client.create_user(user_params)
+      require 'pry'; binding.pry
+      #{"userId"=>"2AAABLblqZhAwNIN4aYZD2dn20cHAhVXNpOiRJogUqzE_XvDV_4pSUDOpWAkrFtcQIbdAZGdiLi8*"}
+    end
   end
 
 end
