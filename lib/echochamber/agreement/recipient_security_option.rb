8282bc4cbf4cb7e209341d297e96b1e82a90b48d
Cath Thomas <goishi.san@gmail.com>
2014-08-18 03:21:25 -0400

    Refine documentation

--- lib/echochamber/agreement/recipient_security_option.rb
+++ lib/echochamber/agreement/recipient_security_option.rb
@@ -7,7 +7,7 @@ module Echochamber
     #
     # @param [Hash] params SYMBOL-referenced Hash.
     # @option params [String] :authenticationMethod ['INHERITED_FROM_DOCUMENT' or 'KBA' or 'PASSWORD' or 'WEB_IDENTITY' or 'PHONE' or 'NONE']: The authentication method for the recipients to have access to view and sign the document. (REQUIRED)
-    # @option params [Array] :phoneInfos The phoneInfo required for the recipient to view and sign the document.  Populate with instances of [Echochamber::PhoneInfo]
+    # @option params [Array] :phoneInfos The phoneInfo required for the recipient to view and sign the document.  Populate with instances of {Echochamber::PhoneInfo Echochamber::PhoneInfo}
     # @option params [String] :password The password required for the recipient to view and sign the document.
     # @return [Echochamber::RecipientSecurityOption]
 
--- lib/echochamber/agreement/url_file_info.rb
+++ lib/echochamber/agreement/url_file_info.rb
@@ -3,10 +3,10 @@ module Echochamber
 
     # Validates UrlFileInfo parameters
     #
-    # @param params [Hash] SYMBOL-referenced Hash 
-    # @option url [String] A publicly accessible URL for retrieving the raw file content. HTTP authentication is supported using standard embedded syntax - i.e. http://username:password@your.server.com/path/to/file (REQUIRED)
-    # @option mimeType [String] The mime type of the referenced file, used to determine if the file can be accepted and the necessary conversion steps can be performed.
-    # @option name [String]  The original system file name of the document being sent - used to name attachments, and to infer the mime type if one is not explicitly specified
+    # @param [Hash] :params SYMBOL-referenced Hash 
+    # @option params [String] :url A publicly accessible URL for retrieving the raw file content. HTTP authentication is supported using standard embedded syntax - i.e. http://username:password@your.server.com/path/to/file (REQUIRED)
+    # @option params [String] :mimeType The mime type of the referenced file, used to determine if the file can be accepted and the necessary conversion steps can be performed.
+    # @option params [String] :name The original system file name of the document being sent - used to name attachments, and to infer the mime type if one is not explicitly specified
     def initialize(params)
       require_keys([:url], params)
       merge! params
